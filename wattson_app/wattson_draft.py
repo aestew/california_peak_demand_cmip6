a6070df9af53087cc87d3324e6cb4a627bccb13f
AESTEW <asteward@berkeley.edu>
2026-04-07 21:00:38 -0700

    labels

--- wattson_app/wattson_draft.py
+++ wattson_app/wattson_draft.py
@@ -582,8 +582,8 @@ with map_col:
             + f"<span style='font-size:12px'>{scenario}</span>"
             + "<br><span style='font-size:2px'> </span><br>"
             + f"<span style='font-size:18px'>{color_metric}: %{{z:,.1f}}{unit}</span>"
-            + "<br><span style='font-size:2px'> </span><br>"
-            + "<span style='font-size:16px'>2025 baseline: %{customdata[0]:,.0f} MWh</span>"
+            #+ "<br><span style='font-size:2px'> </span><br>"
+            + "<span style='font-size:18px'>2025 baseline: %{customdata[0]:,.0f} MWh</span>"
         )
     )
     fig_map.update_layout(
